18c22a6660f9e6bb0b378bc45a32ed97eb9ee3d0
Tadayoshi Sato <tadayosi@gmail.com>
2014-04-25 16:22:45 +0900

    Improved 'soapproxy-wsdltrans' sample

--- soapproxy-wsdltrans/esb/src/main/java/com/redhat/samples/esb/SoapAddressTransformer.java
+++ soapproxy-wsdltrans/esb/src/main/java/com/redhat/samples/esb/SoapAddressTransformer.java
@@ -1,5 +1,6 @@
 package com.redhat.samples.esb;
 
+import org.apache.log4j.Logger;
 import org.milyn.container.ExecutionContext;
 import org.milyn.delivery.dom.DOMVisitAfter;
 import org.milyn.xml.DomUtils;
@@ -7,13 +8,22 @@ import org.w3c.dom.Element;
 
 public class SoapAddressTransformer implements DOMVisitAfter {
 
-  private static final String SOAP_ADDRESS = "soap-address";
+  private static final Logger LOGGER          = Logger.getLogger(SoapAddressTransformer.class);
+
+  private static final String SOAP_ADDRESS    = "soap-address";
+  private static final String SCHEMA_LOCATION = "schema-location";
 
   @Override
   public void visitAfter(Element element, ExecutionContext executionContext) {
-    if (!"address".equals(DomUtils.getName(element))) return;
-    String soapAddress = (String) executionContext.getConfigParameter(SOAP_ADDRESS);
-    element.setAttribute("location", soapAddress.toString());
+    String name = DomUtils.getName(element);
+    if ("import".equals(name)) {
+      LOGGER.info("original schemaLocation = " + element.getAttribute("schemaLocation"));
+      String schemaLocation = (String) executionContext.getConfigParameter(SCHEMA_LOCATION);
+      element.setAttribute("schemaLocation", schemaLocation.toString());
+    } else if ("address".equals(name)) {
+      String soapAddress = (String) executionContext.getConfigParameter(SOAP_ADDRESS);
+      element.setAttribute("location", soapAddress.toString());
+    }
   }
 
 }
